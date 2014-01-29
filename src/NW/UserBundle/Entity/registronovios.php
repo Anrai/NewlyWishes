169bfa58af2b9362068c037c15f8368a1c5f2fed
anrai <anrai@me.com>
2014-01-28 21:56:33 -0600

    Registro de novios mejorando

---
+++ src/NW/UserBundle/Entity/registronovios.php
@@ -0,0 +1,397 @@
+<?php
+// src/NW/UserBundle/Entity/registronovios.php
+namespace NW\UserBundle\Entity;
+ 
+class registronovios
+{
+	// 
+    protected $noviaNombre;
+ 
+ 	// 
+    protected $noviaSNombre;
+
+    // 
+    protected $noviaAPaterno;
+
+    // 
+    protected $noviaAMaterno;
+
+    // 
+    protected $noviaEMail;
+
+    // 
+    protected $noviaLada;
+
+    // 
+    protected $noviaTelefono;
+
+    // 
+    protected $noviaCelular;
+
+    // 
+    protected $noviaDireccion;
+
+    // 
+    protected $noviaPais;
+
+    // 
+    protected $noviaEstado;
+
+    // 
+    protected $noviaCiudad;
+
+    // 
+    protected $noviaCP;
+
+    //
+    protected $novioNombre;
+ 
+ 	// 
+    protected $novioSNombre;
+
+    // 
+    protected $novioAPaterno;
+
+    // 
+    protected $novioAMaterno;
+
+    // 
+    protected $novioEMail;
+
+    // 
+    protected $novioLada;
+
+    // 
+    protected $novioTelefono;
+
+    // 
+    protected $novioCelular;
+
+    // 
+    protected $novioDireccion;
+
+    // 
+    protected $novioPais;
+
+    // 
+    protected $novioEstado;
+
+    // 
+    protected $novioCiudad;
+
+    // 
+    protected $novioCP;
+ 
+    // 
+    public function getnoviaNombre()
+    {
+        return $this->noviaNombre;
+    }
+ 
+ 	// 
+    public function getnoviaSNombre()
+    {
+        return $this->noviaSNombre;
+    }
+
+    // 
+    public function getnoviaAPaterno()
+    {
+        return $this->noviaAPaterno;
+    }
+
+    // 
+    public function getnoviaAMaterno()
+    {
+        return $this->noviaAMaterno;
+    }
+
+    // 
+    public function getnoviaEMail()
+    {
+        return $this->noviaEMail;
+    }
+
+    // 
+    public function getnoviaLada()
+    {
+        return $this->noviaLada;
+    }
+
+    // 
+    public function getnoviaTelefono()
+    {
+        return $this->noviaTelefono;
+    }
+
+    // 
+    public function getnoviaCelular()
+    {
+        return $this->noviaCelular;
+    }
+
+    // 
+    public function getnoviaDireccion()
+    {
+        return $this->noviaDireccion;
+    }
+
+    // 
+    public function getnoviaPais()
+    {
+        return $this->noviaPais;
+    }
+
+    // 
+    public function getnoviaEstado()
+    {
+        return $this->noviaEstado;
+    }
+
+    // 
+    public function getnoviaCiudad()
+    {
+        return $this->noviaCiudad;
+    }
+
+    // 
+    public function getnoviaCP()
+    {
+        return $this->noviaCP;
+    }
+
+    //
+    public function getnovioNombre()
+    {
+        return $this->novioNombre;
+    }
+ 
+ 	// 
+    public function getnovioSNombre()
+    {
+        return $this->novioSNombre;
+    }
+
+    // 
+    public function getnovioAPaterno()
+    {
+        return $this->novioAPaterno;
+    }
+
+    // 
+    public function getnovioAMaterno()
+    {
+        return $this->novioAMaterno;
+    }
+
+    // 
+    public function getnovioEMail()
+    {
+        return $this->novioEMail;
+    }
+
+    // 
+    public function getnovioLada()
+    {
+        return $this->novioLada;
+    }
+
+    // 
+    public function getnovioTelefono()
+    {
+        return $this->novioTelefono;
+    }
+
+    // 
+    public function getnovioCelular()
+    {
+        return $this->novioCelular;
+    }
+
+    // 
+    public function getnovioDireccion()
+    {
+        return $this->novioDireccion;
+    }
+
+    // 
+    public function getnovioPais()
+    {
+        return $this->novioPais;
+    }
+
+    // 
+    public function getnovioEstado()
+    {
+        return $this->novioEstado;
+    }
+
+    // 
+    public function getnovioCiudad()
+    {
+        return $this->novioCiudad;
+    }
+
+    // 
+    public function getnovioCP()
+    {
+        return $this->novioCP;
+    }
+
+    // 
+    public function setnoviaNombre($noviaNombre)
+    {
+        $this->noviaNombre = $noviaNombre;
+    }
+ 
+ 	// 
+    public function setnoviaSNombre($noviaSNombre)
+    {
+        $this->noviaSNombre = $noviaSNombre;
+    }
+
+    // 
+    public function setnoviaAPaterno($noviaAPaterno)
+    {
+        $this->noviaAPaterno = $noviaAPaterno;
+    }
+
+    // 
+    public function setnoviaAMaterno($noviaAMaterno)
+    {
+        $this->noviaAMaterno = $noviaAMaterno;
+    }
+
+    // 
+    public function setnoviaEMail($noviaEMail)
+    {
+        $this->noviaEMail = $noviaEMail;
+    }
+
+    // 
+    public function setnoviaLada($noviaLada)
+    {
+        $this->noviaLada = $noviaLada;
+    }
+
+    // 
+    public function setnoviaTelefono($noviaTelefono)
+    {
+        $this->noviaTelefono = $noviaTelefono;
+    }
+
+    // 
+    public function setnoviaCelular($noviaCelular)
+    {
+        $this->noviaCelular = $noviaCelular;
+    }
+
+     // 
+    public function setnoviaDireccion($noviaDireccion)
+    {
+        $this->noviaDireccion = $noviaDireccion;
+    }
+
+    // 
+    public function setnoviaPais($noviaPais)
+    {
+        $this->noviaPais = $noviaPais;
+    }
+
+    // 
+    public function setnoviaEstado($noviaEstado)
+    {
+        $this->noviaEstado = $noviaEstado;
+    }
+
+    // 
+    public function setnoviaCiudad($noviaCiudad)
+    {
+        $this->noviaCiudad = $noviaCiudad;
+    }
+
+    // 
+    public function setnoviaCP($noviaCP)
+    {
+        $this->noviaCP = $noviaCP;
+    }
+
+    //
+    public function setnovioNombre($novioNombre)
+    {
+        $this->novioNombre = $novioNombre;
+    }
+ 
+ 	// 
+    public function setnovioSNombre($novioSNombre)
+    {
+        $this->novioSNombre = $novioSNombre;
+    }
+
+    // 
+    public function setnovioAPaterno($novioAPaterno)
+    {
+        $this->novioAPaterno = $novioAPaterno;
+    }
+
+    // 
+    public function setnovioAMaterno($novioAMaterno)
+    {
+        $this->novioAMaterno = $novioAMaterno;
+    }
+
+    // 
+    public function setnovioEMail($novioEMail)
+    {
+        $this->novioEMail = $novioEMail;
+    }
+
+    // 
+    public function setnovioLada($novioLada)
+    {
+        $this->novioLada = $novioLada;
+    }
+
+    // 
+    public function setnovioTelefono($novioTelefono)
+    {
+        $this->novioTelefono = $novioTelefono;
+    }
+
+    // 
+    public function setnovioCelular($novioCelular)
+    {
+        $this->novioCelular = $novioCelular;
+    }
+
+    // 
+    public function setnovioDireccion($novioDireccion)
+    {
+        $this->novioDireccion = $novioDireccion;
+    }
+
+    // 
+    public function setnovioPais($novioPais)
+    {
+        $this->novioPais = $novioPais;
+    }
+
+    // 
+    public function setnovioEstado($novioEstado)
+    {
+        $this->novioEstado = $novioEstado;
+    }
+
+    // 
+    public function setnovioCiudad($novioCiudad)
+    {
+        $this->novioCiudad = $novioCiudad;
+    }
+
+    // 
+    public function setnovioCP($novioCP)
+    {
+        $this->novioCP = $novioCP;
+    }
+ 
+}
