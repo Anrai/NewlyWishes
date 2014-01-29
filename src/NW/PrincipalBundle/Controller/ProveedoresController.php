590b0890040ae408e2e434d9fb187f637ac787ec
anrai <anrai@me.com>
2014-01-28 22:03:40 -0600

    Activando direccion de plantillas de proveedores

--- src/NW/PrincipalBundle/Controller/ProveedoresController.php
+++ src/NW/PrincipalBundle/Controller/ProveedoresController.php
@@ -20,4 +20,39 @@ class ProveedoresController extends Controller
     {
         return $this->render('NWPrincipalBundle:Proveedores:misproductos.html.twig');
     }
+
+    public function misbannersAction()
+    {
+        return $this->render('NWPrincipalBundle:Proveedores:misbanners.html.twig');
+    }
+
+    public function misanunciosAction()
+    {
+        return $this->render('NWPrincipalBundle:Proveedores:misanuncios.html.twig');
+    }
+
+    public function misresenasAction()
+    {
+        return $this->render('NWPrincipalBundle:Proveedores:misresenas.html.twig');
+    }
+
+    public function miestadodecuentaAction()
+    {
+        return $this->render('NWPrincipalBundle:Proveedores:miestadodecuenta.html.twig');
+    }
+
+    public function miscodigosAction()
+    {
+        return $this->render('NWPrincipalBundle:Proveedores:miscodigos.html.twig');
+    }
+
+    public function miinformacionbancariaAction()
+    {
+        return $this->render('NWPrincipalBundle:Proveedores:miinformacionbancaria.html.twig');
+    }
+
+    public function tutorialesAction()
+    {
+        return $this->render('NWPrincipalBundle:Proveedores:mtutoriales.html.twig');
+    }
 }
