5e5108accc48dfbcb3cb96a65a433c472c2467b8
anrai <anrai@me.com>
2014-02-06 00:42:06 -0600

    Login novios y recuperacion de administrador

--- src/NW/PrincipalBundle/Controller/ProveedoresController.php
+++ src/NW/PrincipalBundle/Controller/ProveedoresController.php
@@ -6,11 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProveedoresController extends Controller
 {
-    public function loginAction()
-    {
-        return $this->render('NWPrincipalBundle:Proveedores:login.html.twig');
-    }
-
     public function micuentaAction()
     {
         return $this->render('NWPrincipalBundle:Proveedores:micuenta.html.twig');
--- src/NW/UserBundle/Controller/UserController.php
+++ src/NW/UserBundle/Controller/UserController.php
@@ -12,6 +12,32 @@ use Symfony\Component\Security\Core\SecurityContext;
 
 class UserController extends Controller
 {
+    public function loginnoviosAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+ 
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        }
+        else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            'NWUserBundle:User:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+            )
+        );
+    }
+
     public function registronoviosAction(Request $request)
     {
         // crea un nuevo registro y le asigna algunos datos
