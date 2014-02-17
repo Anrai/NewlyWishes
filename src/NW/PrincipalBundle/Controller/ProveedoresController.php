749c740c02574a6f6702488405d4f471bc942454
anrai <anrai@me.com>
2014-02-16 19:00:18 -0600

    Mi Cuenta, de proveedores ya se puede editar

--- src/NW/PrincipalBundle/Controller/ProveedoresController.php
+++ src/NW/PrincipalBundle/Controller/ProveedoresController.php
@@ -6,34 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+use NW\PrincipalBundle\Form\Type\EdicionProveedorType;
+
+use NW\UserBundle\Entity\registroproveedores;
+
 class ProveedoresController extends Controller
 {
     public function micuentaAction(Request $request)
     {
-        // Datos del proveedor
+        // Obtener usuario y proveedor
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['tipoPersona']=$proveedorOject->getTipoPersona();
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-
-        if ($proveedor['tipoPersona']==='moral')
-        {
-             $proveedor['nombre']=$proveedorOject->getNombreRazon().' '.$proveedorOject->getApellidoPaterno().' '.$proveedorOject->getApellidoMaterno();
-        }
-
-        $proveedor['cuenta']=$proveedorOject->getId();
-        $proveedor['tipoPersona']=$proveedorOject->getTipoPersona();
-        $proveedor['rfc']=$proveedorOject->getRfc();
-        $proveedor['email']=$proveedorOject->getEmail();
-        $proveedor['lada']=$proveedorOject->getLada();
-        $proveedor['telefono']=$proveedorOject->getTelefono();
-        $proveedor['celular']=$proveedorOject->getCelular();
-        $proveedor['direccion']=$proveedorOject->getDireccion();
-        $proveedor['estado']=$proveedorOject->getEstados()->getEstado();
-        $proveedor['ciudad']=$proveedorOject->getCiudad();
-        $proveedor['cp']=$proveedorOject->getCp();
-        // Fin de datos del proveedor
+        // Formulario de edición de datos del proveedor
+        $formProveedor = $this->createForm(new EdicionProveedorType());
 
         // Formulario de cambio de contraseña
         $form=$this->createFormBuilder()
@@ -45,31 +31,96 @@ class ProveedoresController extends Controller
         // No se ha actualizado la contraseña
         $statusForm=false;
 
-        // Recuperando datos del formulario de cambio de contraseña
-        $form->handleRequest($request);
+        // Recuperando formularios
+        if('POST' === $request->getMethod()) {
+ 
+            // ¿El formulario que se envió es el de cambio de contraseña?
+            if ($request->request->has($form->getName())) {
+                // Recuperando datos del formulario de cambio de contraseña
+                $form->handleRequest($request);
 
-        if($form->isValid())
-        {
-            // Codificando la contraseña escrita para después compararla con la original
-            $encoder_service = $this->get('security.encoder_factory');
-            $encoder = $encoder_service->getEncoder($user);
-            $encoder_pass = $encoder->encodePassword($form["oldPass"]->getData(), $user->getSalt());
+                if($form->isValid())
+                {
+                    // Codificando la contraseña escrita para después compararla con la original
+                    $encoder_service = $this->get('security.encoder_factory');
+                    $encoder = $encoder_service->getEncoder($user);
+                    $encoder_pass = $encoder->encodePassword($form["oldPass"]->getData(), $user->getSalt());
 
-            // Verificar que la contraseña escrita sea correcta
-            if($encoder_pass === $user->getPassword())
-            {
-                // Cambiar contraseña del usuario
-                $user->setPlainPassword($form["newPass"]->getData());
-                $this->get('fos_user.user_manager')->updateUser($user,false);
-                $this->getDoctrine()->getEntityManager()->flush();
-                
-                // Ya se actualizó la contraseña
-                $statusForm=true;
-            }            
+                    // Verificar que la contraseña escrita sea correcta
+                    if($encoder_pass === $user->getPassword())
+                    {
+                        // Cambiar contraseña del usuario
+                        $user->setPlainPassword($form["newPass"]->getData());
+                        $this->get('fos_user.user_manager')->updateUser($user,false);
+                        $this->getDoctrine()->getEntityManager()->flush();
+                        
+                        // Ya se actualizó la contraseña
+                        $statusForm=true;
+                    }            
+                }
+            }
+
+            // ¿El formulario que se envió es el de edición de los datos del proveedor?
+            else if ($request->request->has($formProveedor->getName())) {
+
+                // handle the second form
+                $formProveedor->handleRequest($request);
+         
+                if ($formProveedor->isValid()) {
+
+                    // Recuperando datos del proveedor
+                    $ProveedorNew = $formProveedor->getData();
+
+                    // Agregando datos del proveedor
+                    $estadoProveedorNew=$this->getDoctrine()->getRepository('NWPrincipalBundle:Estados')->find($formProveedor["estado"]->getData());
+                    
+                    $proveedorObject->setTipoPersona($formProveedor["tipoPersona"]->getData());
+                    $proveedorObject->setNombreRazon($formProveedor["nombreRazon"]->getData());
+                    $proveedorObject->setApellidoPaterno($formProveedor["apellidoPaterno"]->getData());
+                    $proveedorObject->setApellidoMaterno($formProveedor["apellidoMaterno"]->getData());
+                    $proveedorObject->setRfc($formProveedor["rfc"]->getData());
+                    $proveedorObject->setEMail($formProveedor["email"]->getData());
+                    $proveedorObject->setLada($formProveedor["lada"]->getData());
+                    $proveedorObject->setTelefono($formProveedor["telefono"]->getData());
+                    $proveedorObject->setCelular($formProveedor["celular"]->getData());
+                    $proveedorObject->setDireccion($formProveedor["direccion"]->getData());
+                    $proveedorObject->setEstados($estadoProveedorNew);
+                    $proveedorObject->setCiudad($formProveedor["ciudad"]->getData());
+                    $proveedorObject->setCp($formProveedor["cp"]->getData());
+
+                    // Persistiendo los datos en la base de datos
+                    $em = $this->getDoctrine()->getEntityManager();
+                    $em->persist($proveedorObject);
+                    $em->flush();
+                }
+            }
         }
+
+         // Datos del proveedor
+        $proveedor['tipoPersona']=$proveedorObject->getTipoPersona();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+
+        if ($proveedor['tipoPersona']==='fisica')
+        {
+             $proveedor['nombre']=$proveedorObject->getNombreRazon().' '.$proveedorObject->getApellidoPaterno().' '.$proveedorObject->getApellidoMaterno();
+        }
+
+        $proveedor['cuenta']=$proveedorObject->getId();
+        $proveedor['tipoPersona']=$proveedorObject->getTipoPersona();
+        $proveedor['rfc']=$proveedorObject->getRfc();
+        $proveedor['email']=$proveedorObject->getEmail();
+        $proveedor['lada']=$proveedorObject->getLada();
+        $proveedor['telefono']=$proveedorObject->getTelefono();
+        $proveedor['celular']=$proveedorObject->getCelular();
+        $proveedor['direccion']=$proveedorObject->getDireccion();
+        $proveedor['estado']=$proveedorObject->getEstados()->getEstado();
+        $proveedor['ciudad']=$proveedorObject->getCiudad();
+        $proveedor['cp']=$proveedorObject->getCp();
+        // Fin de datos del proveedor
 
         return $this->render('NWPrincipalBundle:Proveedores:micuenta.html.twig', array(
             'form' => $form->createView(),
+            'formProveedor' => $formProveedor->createView(),
             'proveedor' => $proveedor,
             'statusForm' => $statusForm
         ));
@@ -78,10 +129,10 @@ class ProveedoresController extends Controller
     public function misproductosAction()
     {
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-        $proveedor['cuenta']=$proveedorOject->getId();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+        $proveedor['cuenta']=$proveedorObject->getId();
 
         return $this->render('NWPrincipalBundle:Proveedores:misproductos.html.twig', array(
             'proveedor' => $proveedor,
@@ -91,10 +142,10 @@ class ProveedoresController extends Controller
     public function misbannersAction()
     {
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-        $proveedor['cuenta']=$proveedorOject->getId();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+        $proveedor['cuenta']=$proveedorObject->getId();
 
         return $this->render('NWPrincipalBundle:Proveedores:misbanners.html.twig', array(
             'proveedor' => $proveedor,
@@ -104,10 +155,10 @@ class ProveedoresController extends Controller
     public function misanunciosAction()
     {
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-        $proveedor['cuenta']=$proveedorOject->getId();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+        $proveedor['cuenta']=$proveedorObject->getId();
 
         return $this->render('NWPrincipalBundle:Proveedores:misanuncios.html.twig', array(
             'proveedor' => $proveedor,
@@ -117,10 +168,10 @@ class ProveedoresController extends Controller
     public function misresenasAction()
     {
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-        $proveedor['cuenta']=$proveedorOject->getId();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+        $proveedor['cuenta']=$proveedorObject->getId();
 
         return $this->render('NWPrincipalBundle:Proveedores:misresenas.html.twig', array(
             'proveedor' => $proveedor,
@@ -130,10 +181,10 @@ class ProveedoresController extends Controller
     public function miestadodecuentaAction()
     {
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-        $proveedor['cuenta']=$proveedorOject->getId();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+        $proveedor['cuenta']=$proveedorObject->getId();
 
         return $this->render('NWPrincipalBundle:Proveedores:miestadodecuenta.html.twig', array(
             'proveedor' => $proveedor,
@@ -143,10 +194,10 @@ class ProveedoresController extends Controller
     public function miscodigosAction()
     {
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-        $proveedor['cuenta']=$proveedorOject->getId();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+        $proveedor['cuenta']=$proveedorObject->getId();
 
         return $this->render('NWPrincipalBundle:Proveedores:miscodigos.html.twig', array(
             'proveedor' => $proveedor,
@@ -156,10 +207,10 @@ class ProveedoresController extends Controller
     public function miinformacionbancariaAction()
     {
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-        $proveedor['cuenta']=$proveedorOject->getId();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+        $proveedor['cuenta']=$proveedorObject->getId();
 
         return $this->render('NWPrincipalBundle:Proveedores:miinformacionbancaria.html.twig', array(
             'proveedor' => $proveedor,
@@ -169,10 +220,10 @@ class ProveedoresController extends Controller
     public function tutorialesAction()
     {
         $user=$this->getUser();
-        $proveedorOject=$user->getRegistroproveedores();
+        $proveedorObject=$user->getRegistroproveedores();
 
-        $proveedor['nombre']=$proveedorOject->getNombreRazon();
-        $proveedor['cuenta']=$proveedorOject->getId();
+        $proveedor['nombre']=$proveedorObject->getNombreRazon();
+        $proveedor['cuenta']=$proveedorObject->getId();
 
         return $this->render('NWPrincipalBundle:Proveedores:mtutoriales.html.twig', array(
             'proveedor' => $proveedor,
