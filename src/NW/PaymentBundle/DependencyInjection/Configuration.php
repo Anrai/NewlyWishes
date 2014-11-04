b6feb9145ca2a28a2ae02be056135149504710b9
Sergi Enrique <sergioenrique@me.com>
2014-11-03 23:10:46 -0800

    Ligeras correcciones entre versiones localhost y rombomedia

--- src/NW/PaymentBundle/DependencyInjection/Configuration.php
+++ src/NW/PaymentBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\PaymentBundle\DependencyInjection;
+namespace NW\PaymentBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('acme_payment');
+        $rootNode = $treeBuilder->root('nw_payment');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
